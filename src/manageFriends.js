e524123ca83bef3ae6ce5c780d2c141d7b3c44b9
Cory <corystephens012@gmail.com>
2022-01-18 17:55:53 -0500

    commit tree adb692d54d21480f0cae2e80057213578500a88e

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,21 @@
-export function manageFriends(state, action) {
+export function manageFriends(state= {friends: []}, action) {
   // your code here
+  
+  
+  switch(action.type){
+    case "friends/add":
+      return {
+        ...state
+      }
+      case "friends/remove":
+        return {
+
+        }
+  
+    default: return state
+  
+  
+  
+  
+}
 }
